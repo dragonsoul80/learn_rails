65b0600afc8918ceb000ec7eeec67bd3a9d0e3eb
Toan Tang <ttang80@mail.sfsu.edu>
2016-01-07 17:20:09 -0800

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LearnRails::Application.routes.draw do
 	resources :contacts, only: [:new, :create]
 	resources :visitors, only: [:new, :create]
+	resources :about, only: [:new]
 	root to: 'visitors#new'
 end
